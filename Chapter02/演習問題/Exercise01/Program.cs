f16b917afb7c29b5a0d66e50008262f94eee4fda
37096HiroseShota <oibc37096@ojs.ac.jp>
2026-05-13 09:30:53 +0900

    データ入力対応

---
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -0,0 +1,40 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            //2.1.3
+            var songs = new List<Song>();
+
+            Console.WriteLine("***** 曲の登録 *****");
+            while (true) {
+                Console.Write("曲名：");
+                string? title = Console.ReadLine();
+
+                if (String.Compare(title, "end", ignoreCase:true) == 0) {
+                    break;
+                }
+
+                Console.Write("アーティスト名：");
+                string? artistName = Console.ReadLine();
+
+                Console.Write("演奏時間（秒）：");
+                int lenght = int.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+
+                var song = new Song(title, artistName, lenght);
+                songs.Add(song);
+            }
+            PrintSongs(songs);
+        }
+        //Mainメソッド内の　PrintSongs(songs);　をクリックして
+        //Alt + Enterを押して、メソッドを生成するを選択すると、
+        //以下のメソッドが自動的に生成される
+        //2.1.4
+        private static void PrintSongs(List<Song> songs) {
+            foreach (var song in songs) {
+                Console.WriteLine($"{song.Title},{song.ArtistName},{song.Lenght / 60}:{(song.Lenght % 60):00}");
+            }
+        }
+    }
+}
---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    public class Song {
+        public string Title { get; set; } = string.Empty;
+        public string ArtistName { get; set; } = string.Empty;
+        public int Lenght { get; set; }
+
+        public Song(string title, string artistname, int lenght) {
+            Title = title;
+            ArtistName = artistname;
+            Lenght = lenght;
+        }
+    }
+}
