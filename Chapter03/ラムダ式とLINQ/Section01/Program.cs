1af90b7d59c3af26bf57901e0dc060bbe8b74451
37096HiroseShota <oibc37096@ojs.ac.jp>
2026-05-20 10:34:35 +0900

    P71まで

--- Chapter03/ラムダ式とLINQ/Section01/Program.cs
+++ Chapter03/ラムダ式とLINQ/Section01/Program.cs
@@ -1,25 +1,27 @@
-﻿using Microsoft.VisualBasic;
-
-namespace Section01 {
+﻿namespace Section01 {
     internal class Program {
-        static void Main(string[] args) {
 
+        static void Main(string[] args) {
             var cities = new List<string> {
-               "Tokyo",
-               "New Delhi",
-               "Bangkok",
-               "London",
-               "Paris",
-               "Berlin",
-               "Canberra",
-               "Hong Kong",
+                "Tokyo",
+                "New Delhi",
+                "Bangkok",
+                "London",
+                "Paris",
+                "Berlin",
+                "Canberra",
+                "Hong Kong",
             };
 
-            var name = cities.Find(s => s.Length == 6);
-            Console.WriteLine(name);
-
-            var exists = cities.Exists(s => s[0] == 'o');
+            //以下の条件を満たすものが存在するか調べる
+            //【教科書P134～137を参考に】
+            //・文字数が6文字以上
+            //・o を含む
+            //・最後が n
+            var exists = cities.Exists(s => 6 <= s.Length && s.Contains('o') && s.EndsWith('n'));
             Console.WriteLine(exists);
         }
     }
 }
+
+
