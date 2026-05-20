024e0757f06eaa21095f0fe9d418da99fa077906
37096HiroseShota <oibc37096@ojs.ac.jp>
2026-05-20 09:40:35 +0900

    就活で休んだ分を入力

---
+++ Chapter03/ラムダ式とLINQ/Section01/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var cities = new List<string> {
+               "Tokyo",
+               "New Delhi",
+               "Bangkok",
+               "London",
+               "Paris",
+               "Berlin",
+               "Canberra",
+               "Hong Kong",
+            };
+
+            var name = cities.Find(s => s.Length == 6);
+            Console.WriteLine(name);
+
+            var exists = cities.Exists(s => s[0] == 'o');
+            Console.WriteLine(exists);
+        }
+    }
+}
