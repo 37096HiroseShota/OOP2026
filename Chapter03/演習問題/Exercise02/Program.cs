b886f24f78181eaed51f8fc84f2b115c5a22c043
37096HiroseShota <oibc37096@ojs.ac.jp>
2026-05-21 11:13:50 +0900

    問題3.2.3完成

--- Chapter03/演習問題/Exercise02/Program.cs
+++ Chapter03/演習問題/Exercise02/Program.cs
@@ -38,8 +38,11 @@ namespace Exercise02 {
         }
 
         private static void Exercise2_3(List<string> cities) {
-	        //できたらGitのコメント「問題3.2.3完成」
-	        
+            //できたらGitのコメント「問題3.2.3完成」
+            var query =  cities.Where(c => c.Contains('o')).ToArray();
+            foreach (var item in query) {
+                Console.WriteLine(item);
+            }
         }
 
         private static void Exercise2_4(List<string> cities) {
