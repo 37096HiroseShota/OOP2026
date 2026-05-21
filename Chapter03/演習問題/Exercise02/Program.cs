767be9512d7c34380d895e48476f3cb38ae0cd0c
37096HiroseShota <oibc37096@ojs.ac.jp>
2026-05-21 11:59:34 +0900

    問題3.2.4修正

--- Chapter03/演習問題/Exercise02/Program.cs
+++ Chapter03/演習問題/Exercise02/Program.cs
@@ -54,7 +54,9 @@ namespace Exercise02 {
         private static void Exercise2_4(List<string> cities) {
             //できたらGitのコメント「問題3.2.4完成」
             var query = cities.Where(c => c.StartsWith('B')).Select(c => c.Length);
-            Console.WriteLine(query);
+            foreach (var item in query) {
+                Console.WriteLine(item);
+            }
         }
     }
 }
