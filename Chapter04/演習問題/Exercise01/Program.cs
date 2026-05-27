79a853c9ba8e88b1730fb72438d5545fe2b4e36f
37096HiroseShota <oibc37096@ojs.ac.jp>
2026-05-27 09:43:43 +0900

    4.1.1完成

---
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Linq;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            List<string> langs = [
+                "C#", "Java", "Ruby", "PHP", "Python", "TypeScript",
+                "JavaScript", "Swift", "Go",
+            ];
+
+            Exercise1(langs);
+            Console.WriteLine("---");
+            Exercise2(langs);
+            Console.WriteLine("---");
+            Exercise3(langs);
+        }
+
+        private static void Exercise1(List<string> langs) {
+            foreach (var item in langs) {
+                if (item.Contains('S')) {
+                    Console.WriteLine(item);
+                }
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < langs.Count; i++) {
+                if (langs[i].Contains('S')) {
+                    Console.WriteLine(langs[i]);
+                }
+            }
+
+            Console.WriteLine();
+
+            int count = 0;
+            while (true) {
+                if (langs[count].Contains('S')) {
+                    Console.WriteLine(langs[count]);
+                }
+                if (count >= (langs.Count - 1)) {
+                    break;
+                }
+                count++;
+            }
+        }
+
+        private static void Exercise2(List<string> langs) {
+        }
+
+        private static void Exercise3(List<string> langs) {
+        }
+    }
+}
---
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -0,0 +1,6 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
