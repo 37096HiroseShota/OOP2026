e8142c78b839463357e16db9783661e6d0105cc6
37096HiroseShota <oibc37096@ojs.ac.jp>
2026-05-27 10:28:13 +0900

    4.2.1完成

--- Chapter04/演習問題/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -1,6 +1,42 @@
-﻿namespace Exercise02 {
+﻿
+using System.ComponentModel.Design;
+
+namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
+            Console.WriteLine(" --- 4.2.1 ---");
+            Exercise1();
+            Console.WriteLine("\n --- 4.2.2 ---");
+            Exercise2();
+            Console.WriteLine("\n --- 4.2.3 ---");
+            Exercise3();
+        }
+
+        private static void Exercise1() {
+            //if-else文を使用
+            string? inputNumber = Console.ReadLine();
+            int.TryParse(inputNumber, out var number);
+
+            if (number < 0) {
+                Console.WriteLine(number);
+            } else if (number < 100) {
+                Console.WriteLine(number * 2);
+            } else if (number < 500) {
+                Console.WriteLine(number * 3);
+            } else {
+                Console.WriteLine(number);
+            }
+        }
+
+        private static void Exercise2() {
+            //switch文を使用
+
+        }
+
+        private static void Exercise3() {
+            //switch式を使用
+
+
         }
     }
 }
