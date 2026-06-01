7370fb5fcdc588607db2171e1587e7551bec5792
37096HiroseShota <oibc37096@ojs.ac.jp>
2026-06-01 10:17:54 +0900

    5.1.1と5.1.2完成

--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -7,11 +7,19 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Exercise01 {
     //5.1.1
-    public class YearMonth(int year, int month) {
-        public int Year { get; init; } = year;
-        public int Month { get; init; } = month;
+    public class YearMonth {
+        public required int Year { get; init; }
+        public required int Month { get; init; }
+
+        //5.1.2
         public bool IS21Century { get; init; }
 
-        public YearMonth()
+        public YearMonth(int year, int month) {
+            Year = year;
+            Month = month;
+            if (Year > 2000 && Year < 2101) {
+                IS21Century = true;
+            }
+        }
     }
 }
