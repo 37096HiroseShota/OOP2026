8eca703958592cf86a02ffc6ee211f32969e12e9
37096HiroseShota <oibc37096@ojs.ac.jp>
2026-06-01 10:01:02 +0900

    確認

---
+++ Chapter05/演習問題/Exercise01/Program.cs
@@ -0,0 +1,6 @@
+﻿namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
---
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Exercise01 {
+    //5.1.1
+    public class YearMonth(int year, int month) {
+        public int Year { get; init; } = year;
+        public int Month { get; init; } = month;
+        public bool IS21Century { get; init; }
+
+        public YearMonth()
+    }
+}
