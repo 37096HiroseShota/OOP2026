afc03790acf25ac229d2ac8c25f2e28cf3142dea
37096HiroseShota <oibc37096@ojs.ac.jp>
2026-06-01 10:56:35 +0900

    5.1修正と完成

--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -7,22 +7,17 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Exercise01 {
     //5.1.1
-    public class YearMonth {
-        public int Year { get; init; }
-        public int Month { get; init; }
+    public class YearMonth(int year, int month) {
+        public int Year { get; init; } = year;
+        public int Month { get; init; } = month;
 
         //5.1.2
-        public bool IS21Century { get; init; }
+        //設定されている西暦が21世紀か判定する
+        //Yearが2001～2100年の間ならtrue、それ以外ならfalseを返す
+        //以下はメソッドの例(return true/false　は使用しないで記述するP91参照)
+        public bool IS21Century => Year > 2000 && Year < 2101;
 
-        public YearMonth(int year, int month) {
-            Year = year;
-            Month = month;
-            if (Year > 2000 && Year < 2101) {
-                IS21Century = true;
-            }
-        }
-
-        //5.1.2
+        //5.1.3
         public int AddOneMonth() {
             var yearmonth = new YearMonth(this.Year, this.Month + 1);
             if (yearmonth.Month < 13) {
@@ -30,5 +25,8 @@ namespace Exercise01 {
             }
             return yearmonth.Month - 12;
         }
+
+        //5.1.4
+        public override string ToString() => $"{Year}年{Month}月";
     }
 }
