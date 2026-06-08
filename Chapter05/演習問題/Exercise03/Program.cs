d6ce802bc7ab665b06c416e9e2b69b89f8573bc8
37096HiroseShota <oibc37096@ojs.ac.jp>
2026-06-08 11:05:25 +0900

    クラスとレコードの比較

---
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -0,0 +1,37 @@
+﻿using Exercise01;
+
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            var a = new YearMonth(2000, 12);
+            var b = new YearMonth(2000, 12);
+
+            #region class
+            //別のオブジェクトのため、等しくない
+            if (a == b) {
+                Console.WriteLine("aとbは等しい");
+            } else {
+                Console.WriteLine("aとbは等しくない");
+            }
+
+            b = a;
+
+            //同じ参照のため、等しい
+            if (a == b) {
+                Console.WriteLine("aとbは等しい");
+            } else {
+                Console.WriteLine("aとbは等しくない");
+            }
+            #endregion
+
+            #region record
+            //値が同じであるため、等しい
+            if (a == b) {
+                Console.WriteLine("aとbは等しい");
+            } else {
+                Console.WriteLine("aとbは等しくない");
+            }
+            #endregion
+        }
+    }
+}
