6dc1cca538c2edc2a9f72ceebbad721eb0c5aee7
37096HiroseShota <oibc37096@ojs.ac.jp>
2026-06-10 10:24:02 +0900

    5.2変数名の変更

--- Chapter06/演習問題/Exercise02/Program.cs
+++ Chapter06/演習問題/Exercise02/Program.cs
@@ -2,9 +2,9 @@
     internal class Program {
         static void Main(string[] args) {
             Console.Write("整数文字列:");
-            var input = Console.ReadLine();
+            var str = Console.ReadLine();
 
-            if (int.TryParse(input, out var num)) {
+            if (int.TryParse(str, out var num)) {
                 Console.WriteLine(num.ToString("#,0"));
             } else {
                 Console.WriteLine("整数文字列ではありません");
