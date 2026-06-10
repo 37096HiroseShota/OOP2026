d6b9ee08879e9181fea94b0223dd05580af568a4
37096HiroseShota <oibc37096@ojs.ac.jp>
2026-06-10 09:53:27 +0900

    5.1完成

---
+++ Chapter06/演習問題/Exercise01/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var input1 = Console.ReadLine();
+            var input2 = Console.ReadLine();
+
+            if (String.Compare(input1, input2, ignoreCase: true) == 0) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+        }
+    }
+}
