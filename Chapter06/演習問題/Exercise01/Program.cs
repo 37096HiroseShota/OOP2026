e9c452683ab71ec9742e745a69c7e7599b21f625
37096HiroseShota <oibc37096@ojs.ac.jp>
2026-06-10 10:23:18 +0900

    5.1改良

--- Chapter06/演習問題/Exercise01/Program.cs
+++ Chapter06/演習問題/Exercise01/Program.cs
@@ -1,10 +1,13 @@
 ﻿namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
-            var input1 = Console.ReadLine();
-            var input2 = Console.ReadLine();
+            Console.Write("文字列1:");
+            var str1 = Console.ReadLine();
 
-            if (String.Compare(input1, input2, ignoreCase: true) == 0) {
+            Console.Write("文字列2:");
+            var str2 = Console.ReadLine();
+
+            if (String.Compare(str1, str2, ignoreCase: true) == 0) {
                 Console.WriteLine("等しい");
             } else {
                 Console.WriteLine("等しくない");
