a57dc81c505e825bff271c4403f4a324e45c734b
37096HiroseShota <oibc37096@ojs.ac.jp>
2026-06-10 10:20:48 +0900

    6.2完成

---
+++ Chapter06/演習問題/Exercise02/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("整数文字列:");
+            var input = Console.ReadLine();
+
+            if (int.TryParse(input, out var num)) {
+                Console.WriteLine(num.ToString("#,0"));
+            } else {
+                Console.WriteLine("整数文字列ではありません");
+            }
+        }
+    }
+}
