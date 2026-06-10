df52b901e45af3618cdb0c9b9a0f9e4d4eeb853d
37096HiroseShota <oibc37096@ojs.ac.jp>
2026-06-10 12:05:09 +0900

    6.5まで

--- Chapter06/演習問題/Exercise03/Program.cs
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Text;
+
 namespace Exercise03 {
     internal class Program {
         static void Main(string[] args) {
@@ -19,35 +21,43 @@ namespace Exercise03 {
             Console.WriteLine("6.3.5");
             Exercise5(text);
 
-            Console.WriteLine("6.3.99");
+            Console.WriteLine("6.3.6");
             Exercise6(text);
             #endregion
         }
 
         private static void Exercise1(string text) {
-            var count = 0;
-            foreach (var c in text) {
-                if (c.Equals(' ') || c.Equals('　')) {
-                    count++;
-                }
-            }
-            Console.WriteLine($"{count}文字");
+            Console.WriteLine($"空白数:{text.Count(c => c == ' ')}");
         }
 
         private static void Exercise2(string text) {
+            Console.Write("検索:");
+            var search = Console.ReadLine();
 
+            Console.Write("置換:");
+            var replace = Console.ReadLine();
+
+            var replaced = text.Replace(search, replace);
+            Console.WriteLine($"置換語文字列:{replaced}");
         }
 
         private static void Exercise3(string text) {
-
+            string[] words = text.Split(' ');
+            var sb = new StringBuilder();
+            foreach (var word in words) {
+                sb.Append(word + ' ');
+            }
+            Console.WriteLine(sb.ToString());
         }
 
         private static void Exercise4(string text) {
-
+            string[] words = text.Split(' ');
+            Console.WriteLine("単語数" + words.Length);
         }
 
         private static void Exercise5(string text) {
-
+            string[] words = text.Split(' ');
+            words.Where(c => c.Length >= 4).ToList().ForEach(s => Console.WriteLine(s));
         }
 
         private static void Exercise6(string text) {
