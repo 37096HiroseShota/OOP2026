b46cccece192f83d48977cb83044fe1d720cb55f
37096HiroseShota <oibc37096@ojs.ac.jp>
2026-06-11 10:49:04 +0900

    辞書（ディクショナリ）を使った集計

--- Chapter06/演習問題/Exercise03/Program.cs
+++ Chapter06/演習問題/Exercise03/Program.cs
@@ -9,19 +9,19 @@ namespace Exercise03 {
             Console.WriteLine("6.3.1");
             Exercise1(text);
 
-            Console.WriteLine("6.3.2");
+            Console.WriteLine("\n6.3.2");
             Exercise2(text);
 
-            Console.WriteLine("6.3.3");
+            Console.WriteLine("\n6.3.3");
             Exercise3(text);
 
-            Console.WriteLine("6.3.4");
+            Console.WriteLine("\n6.3.4");
             Exercise4(text);
 
-            Console.WriteLine("6.3.5");
+            Console.WriteLine("\n6.3.5");
             Exercise5(text);
 
-            Console.WriteLine("6.3.6");
+            Console.WriteLine("\n6.3.6");
             Exercise6(text);
             #endregion
         }
@@ -38,29 +38,52 @@ namespace Exercise03 {
             var replace = Console.ReadLine();
 
             var replaced = text.Replace(search, replace);
-            Console.WriteLine($"置換語文字列:{replaced}");
+            Console.WriteLine($"\n置換語文字列:{replaced}");
         }
 
         private static void Exercise3(string text) {
             string[] words = text.Split(' ');
             var sb = new StringBuilder();
             foreach (var word in words) {
-                sb.Append(word + ' ');
+                sb.Append(word);
             }
             Console.WriteLine(sb.ToString());
         }
 
         private static void Exercise4(string text) {
-            string[] words = text.Split(' ');
-            Console.WriteLine("単語数" + words.Length);
+            Console.WriteLine($"単語数:{text.Split(' ').Length}");
         }
 
         private static void Exercise5(string text) {
-            string[] words = text.Split(' ');
-            words.Where(c => c.Length >= 4).ToList().ForEach(s => Console.WriteLine(s));
+            text.Split(' ').Where(s => 4 >= s.Length).ToList().ForEach(Console.WriteLine);
         }
 
+        //アルファベットの数をカウントして表示する
         private static void Exercise6(string text) {
+            var str = text.ToLower().Replace(" ", "");
+
+            //辞書（ディクショナリ）を使った集計
+            var alphDicCount = Enumerable.Range('a', 26).
+                ToDictionary(num => ((char)num).ToString(), num => 0);
+
+            var dict = new SortedDictionary<char, int>();
+            foreach (var c in str) {
+                if (dict.ContainsKey(c)) {
+                    dict[c]++;      //登録されている場合
+                } else {
+                    dict[c] = 1;    //未登録の場合
+                }
+            }
+            foreach (var word in dict) {
+                Console.WriteLine(word.Key + ":" + word.Value);
+            }
+
+            //配列を用いた集計
+
+
+
+            //'a'から順にカウントして集計
+
 
         }
     }
