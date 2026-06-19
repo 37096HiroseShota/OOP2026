764fab345c105a0128cf18be47ecd5ac60e5d970
37096HiroseShota <oibc37096@ojs.ac.jp>
2026-06-19 10:16:27 +0900

    7.1.3まで

---
+++ Chapter07/演習問題/Exercise01/Program.cs
@@ -0,0 +1,46 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            int[] numbers = [5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35];
+
+            Console.WriteLine("\n7.1.1");
+            Exercise1(numbers);
+
+            Console.WriteLine("\n7.1.2");
+            Exercise2(numbers);
+
+            Console.WriteLine("\n7.1.3");
+            Exercise3(numbers);
+
+            Console.WriteLine("\n7.1.4");
+            Exercise4(numbers);
+
+            Console.WriteLine("\n7.1.5");
+            Exercise5(numbers);
+        }
+
+        private static void Exercise1(int[] numbers) {
+            Console.WriteLine(numbers.Max());
+        }
+
+        private static void Exercise2(int[] numbers) {
+            Console.WriteLine(numbers.LastOrDefault());
+        }
+
+        private static void Exercise3(int[] numbers) {
+            var array = numbers.Select(n => n.ToString("000")).ToArray();
+            foreach (var number in array) {
+                Console.WriteLine(number);
+            }
+        }
+
+        private static void Exercise4(int[] numbers) {
+
+        }
+
+        private static void Exercise5(int[] numbers) {
+            
+        }
+    }
+}
