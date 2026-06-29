abc290342bb119fd5d68f75a3b15d15d3f09cd21
37096HiroseShota <oibc37096@ojs.ac.jp>
2026-06-29 15:49:22 +0900

    Chapter08演習問題開始

---
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Cozy lummox gives smart squid who job pen";
+
+            Exercise01(text);
+            Console.WriteLine();
+            Exercise02(text);
+        }
+
+        private static void Exercise01(string text) {
+            //if('A' <= ch && ch <= 'Z') {
+
+            //}
+        }
+
+        private static void Exercise02(string text) {
+            //SortedDictionary<TKey, TValue>
+        }
+    }
+}
