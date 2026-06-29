f63c2f6db7e1c1fc8cce66947ae9863ba663e002
37096HiroseShota <oibc37096@ojs.ac.jp>
2026-06-29 13:14:25 +0900

    休んでいた分を読み込み

---
+++ Chapter08/Section01/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Section01 {
+    internal class Program {
+        static private Dictionary<string, string> prefOfficeDict = new Dictionary<string, string>();
+
+
+        static void Main(string[] args) {
+            string? pref, prefCaptalLocation;
+
+            Console.WriteLine("県庁所在地の登録【入力終了：Ctrl + 'Z'】");
+
+
+            while (true) {
+                //①都道府県の入力
+                Console.Write("都道府県:");
+                pref = Console.ReadLine();
+
+                if (pref == null) break;    //無限ループを抜ける(Ctrl + 'Z')
+
+                //②県庁所在地の入力
+                Console.Write("県庁所在地:");
+                prefCaptalLocation = Console.ReadLine();
+
+
+
+                //③県庁所在地登録処理
+
+
+
+            }
+        }
+    }
+}
