acf570e02c95637aeed1b41147cc86084c38c965
37096HiroseShota <oibc37096@ojs.ac.jp>
2026-08-31 15:31:31 +0900

    UPDATEで商品を修正する

--- FormApps/SQLiteProductSample/ProductRepository.cs
+++ FormApps/SQLiteProductSample/ProductRepository.cs
@@ -2,6 +2,8 @@ using Microsoft.Data.Sqlite;
 
 namespace SQLiteProductSample;
 
+// Prosuctテーブルに対するDB操作をまとめたクラス
+// CRUD (Create / Read / Update / Delete) を担当する
 public class ProductRepository {
     public List<Product> GetAll() {
         var products = new List<Product>();
@@ -9,7 +11,9 @@ public class ProductRepository {
         using var connectioin = Database.GetConnection();
         connectioin.Open();
 
+        //SQLを実行するためのコマンドオブジェクトを作る
         using var command = connectioin.CreateCommand();
+
         command.CommandText =
         """
         SELECT Id, Name, Price
@@ -17,13 +21,14 @@ public class ProductRepository {
         ORDER BY Id;
         """;
 
+        //SELECTを実行し、複数行の検索結果を読み取る
         using var reader = command.ExecuteReader();
 
         while (reader.Read()) {
             products.Add(new Product {
-                Id = reader.GetInt32(0),
-                Name = reader.GetString(1),
-                Price = reader.GetInt32(2)
+                Id = reader.GetInt32(0), //0列目: Id
+                Name = reader.GetString(1), //1列目: Name
+                Price = reader.GetInt32(2) //2列目: Price
             });
         }
 
@@ -34,7 +39,10 @@ public class ProductRepository {
         using var connection = Database.GetConnection();
         connection.Open();
 
+
+        //SQLを実行するためのコマンドオブジェクトを作る
         using var command = connection.CreateCommand();
+
         command.CommandText =
         """
         INSERT INTO Products (Name, Price)
@@ -43,6 +51,27 @@ public class ProductRepository {
 
         command.Parameters.AddWithValue("$name", name);
         command.Parameters.AddWithValue("$price", price);
+
+        //結果行を返さないSQLを実行する
+        command.ExecuteNonQuery();
+    }
+
+    private void Update(Product product) {
+        using var connection = Database.GetConnection();
+        connection.Open();
+
+        using var command = connection.CreateCommand();
+        command.CommandText =
+        """
+        UPDATE Products
+        SET Name = $name,
+            Price = $price
+        WHERE Id = $id;
+        """;
+
+        command.Parameters.AddWithValue("$name", product.Name);
+        command.Parameters.AddWithValue("$price", product.Price);
+        command.Parameters.AddWithValue("$Id", product.Id);
 
         command.ExecuteNonQuery();
     }
